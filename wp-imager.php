49b46bc74ddba1ad77f3774efd25f2b8e89f8fed
Jany <jany.martelli@shambix.com>
2015-03-30 22:37:37 +0200

    Added Photon support

--- wp-imager.php
+++ wp-imager.php
@@ -3,9 +3,9 @@
 /**
  *	WP Imager
  *
- *	Description			Script for WordPress that provides resizing, output customization and image caching. Can be used inside or outside the loop. If used inside a loop, there is no need to use $exturl, as the script will automatically retrieve an image from the post, following a certain priority pattern: featured image if found, otherwise take one random image from the post. If used outside the loop for any image you want, then $exturl is obviously required.
+ *	Description			Script for WordPress that provides resizing, output customization and image caching. Supports Jetpack Photon. Can be used inside or outside the loop. If used inside a loop, the script will automatically retrieve an image from the post, following a priority pattern: featured image if found, otherwise take one random image from the post. If used outside the loop for any image you want, then $exturl is required.
  *	Released			29.01.2014
- *	Version				1.5
+ *	Version				1.8
  *	License				GPL V3 - http://choosealicense.com/licenses/gpl-v3/
  *  External libs		TimThumb - http://code.google.com/p/timthumb/
  *
@@ -44,9 +44,31 @@
 function wp_imager($width=null, $height=null, $crop=null, $class='', $link=false, $exturl=null, $nohtml=false) {
 	global $post;
 
-	// Prepping stuff
+	// Is Photon on and working? 
+	// https://developer.wordpress.com/docs/photon/api/
+	if( class_exists( 'Jetpack' ) && Jetpack::is_module_active( 'photon' ) ) { // method as of WP/Jetpack versions after 05/22/13
+		$photon = true;
+		remove_filter( 'image_downsize', array( Jetpack_Photon::instance(), 'filter_image_downsize' ) );
+	/*} elseif( class_exists( 'Jetpack' ) && in_array( 'photon', Jetpack::get_active_modules() )) { // legacy mode for older versions
+		$photon = true;
+		remove_filter( 'image_downsize', array( Jetpack_Photon::instance(), 'filter_image_downsize' ) );
+	*/} else {
+		$photon = false;
+	}
+	if($photon && !function_exists( 'jetpack_photon_url' )) echo 'There is something wrong with your Jetpack / Photon module, or your server configuration - Make sure that your website is publicly reachable.';
+
+	// Get attachs
+	$attachments = get_children( array('post_parent' => $post->ID, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'orderby' => 'rand', 'numberposts' => 1) );
+
+	// Defaults
+	$htaccess = true; // htaccess is getting on my nerves, so I'll disable it by default - switch to true if you want to use a custom htaccess for pretty img urls
+	if(!isset($width) || is_null($width) || $width == '') $width = '100';
+	if(!isset($height) || is_null($height) || $width == '') $height = '100';
+	if(!isset($crop) || is_null($crop) || $crop == '') $crop = '1';
+	if($class !== '') $printclass = 'class="'.$class.'" ';
 	$cache = 'cache_img';
 	$thumbnail = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'full');
+	//echo $thumbnail[0];
 
 	// Fix for site url lang edit (WPML)
 	if(array_key_exists('sitepress', $GLOBALS)) {
@@ -62,17 +84,6 @@ function wp_imager($width=null, $height=null, $crop=null, $class='', $link=false
 		$exturl = str_replace(get_bloginfo('url').'/', '', $exturl);
 	}
 	$siteurl = $genurl.'/'.$cache;
-	
-	// Get attachs
-	$attachments = get_children( array('post_parent' => $post->ID, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'orderby' => 'rand', 'numberposts' => 1) );
-	
-	// Defaults
-	$htaccess = true; // htaccess is getting on my nerves, so I'll disable it by default - switch to true if you want to use a custom htaccess for pretty img urls
-	if(!isset($width) || is_null($width) || $width == '') $width = '100';
-	if(!isset($height) || is_null($height) || $width == '') $height = '100';
-	if(!isset($crop) || is_null($crop) || $crop == '') $crop = '1';
-	if($class !== '') $printclass = 'class="'.$class.'" ';
-
 
 	// External image URL
 	if ($exturl) {
@@ -88,21 +99,28 @@ function wp_imager($width=null, $height=null, $crop=null, $class='', $link=false
 		// Fix for site url lang edit (WPML)
 		if(defined('ICL_LANGUAGE_CODE') && ICL_LANGUAGE_CODE !== $deflang) {
 			$thumb2part = str_replace(get_bloginfo('url').'/'.$lang.'/', '', $thumbnail[0]);
-			$thumb2part = str_replace($genurl.'/', '', $thumb2part);
-			
+			$thumb2part = str_replace($genurl.'/', '', $thumb2part);		
 		} else {
 			$thumb2part = str_replace(get_bloginfo('url'), '', $thumbnail[0]);
 		}
+		// Fix for Photon
+		if($photon) {
+			$thumb2part = str_replace('http://','', $thumbnail[0]);
+		}
 
 		if ($nohtml) {
-			if ($htaccess) {
-				$output = ''.$siteurl.'/r/'.$width.'x'.$height.'-'.$crop.'/i/'.$thumb2part.'';
+			if($photon) {
+					$output = 'http://i1.wp.com/'.$thumb2part.'?resize='.$width.','.$height.'&amp;quality=100&amp;strip=all';
+			} elseif ($htaccess) {
+				$output = $siteurl.'/r/'.$width.'x'.$height.'-'.$crop.'/i/'.$thumb2part;
 			} else {
-				$output = ''.$siteurl.'/tt.php?src='.$thumb2part.'&w='.$width.'&h='.$height.'&zc='.$crop.'&q=100';
+				$output = $siteurl.'/tt.php?src='.$thumb2part.'&w='.$width.'&h='.$height.'&zc='.$crop.'&q=100';
 			}
 		} else {
 			if($link) $output .= '<a href="'.get_permalink($post->ID).'" title="'.$post->post_title.'">';
-			if ($htaccess) {
+			if($photon) {
+				$output .= '<img src="http://i1.wp.com/'.$thumb2part.'?resize='.$width.','.$height.'&amp;quality=100&amp;strip=all" alt="'.$post->post_title.'" '.$printclass.' />';
+			} elseif ($htaccess) {
 				$output .= '<img src="'.$siteurl.'/r/'.$width.'x'.$height.'-'.$crop.'/i/'.$thumb2part.'" alt="'.$post->post_title.'" '.$printclass.' />';
 			} else {
 				$output .= '<img src="'.$siteurl.'/tt.php?src='.$thumb2part.'&w='.$width.'&h='.$height.'&zc='.$crop.'&q=100" alt="'.$post->post_title.'" '.$printclass.' />';
@@ -122,15 +140,24 @@ function wp_imager($width=null, $height=null, $crop=null, $class='', $link=false
 			} else {
 				$img2part = str_replace(get_bloginfo('url'), '', $img_url);
 			}
+			// Fix for Photon
+			if($photon) {
+				$img2part = str_replace('http://','', $img_url);
+			}
+				
 			if ($nohtml) {
-				if ($htaccess) {
-					$output = ''.$siteurl.'/r/'.$width.'x'.$height.'-'.$crop.'/i/'.$img2part.'';
+				if($photon) {
+					$output = 'http://i1.wp.com/'.$img2part.'?resize='.$width.','.$height.'&amp;quality=100&amp;strip=all';
+				} elseif ($htaccess) {
+					$output = $siteurl.'/r/'.$width.'x'.$height.'-'.$crop.'/i/'.$img2part;
 				} else {
 					$output = ''.$siteurl.'/tt.php?src='.$img2part.'$w='.$width.'&h='.$height.'&zc='.$crop.'&q=100';
 				}
 			} else {
 				if($link) $output .= '<a href="'.get_permalink($post->ID).'" title="'.$post->post_title.'">';
-				if ($htaccess) {
+				if($photon) {
+					$output .='<img src="http://i1.wp.com/'.$img2part.'?resize='.$width.','.$height.'&amp;quality=100&amp;strip=all" alt="'.$post->post_title.'" '.$printclass.' />';
+				} elseif ($htaccess) {
 					$output .='<img src="'.$siteurl.'/r/'.$width.'x'.$height.'-'.$crop.'/i/'.$img2part.'" alt="'.$post->post_title.'" '.$printclass.' />';
 				} else {
 					$output .='<img src="'.$siteurl.'/tt.php?src='.$img2part.'$w='.$width.'&h='.$height.'&zc='.$crop.'&q=100" alt="'.$post->post_title.'" '.$printclass.' />';
@@ -140,6 +167,11 @@ function wp_imager($width=null, $height=null, $crop=null, $class='', $link=false
 			return $output;
 			break;
 		}
-	} 	
+	}
+
+	// Since Photon keeps changing automatically all urls, we had to disable it, let's reactivate it now
+	if ( $photon ) {
+		add_filter( 'image_downsize', array( Jetpack_Photon::instance(), 'filter_image_downsize' ), 10, 3 ); 
+	}
 }
 ?>
